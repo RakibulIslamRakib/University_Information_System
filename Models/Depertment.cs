61925942dc8f0f666d9e41257763db9c678a22b7
RakibulIslamRakib <rakibulr14@cse.pstu.ac.bd>
2022-08-29 15:40:08 +0600

    Add project files.

---
+++ Models/Depertment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University_Information_System.Models
+{
+    public class Depertment
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string DeptName { get; set; }
+        public virtual ICollection<Teacher> Teachers { get; set; }
+
+
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public Teacher? CreatedBy { get; set; }
+        public Teacher? UpdatedBy { get; set; }
+
+
+    }
+}
+    
---
+++ Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace University_Information_System.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string TeachersName { get; set; }
+
+        public virtual Depertment Depertment { get; set; }
+    }
+}
